720aaab252bc7774079d4493c40196dbf1ed8aa7
Tran McFarland Nguyen <73681436+Tranimal-N@users.noreply.github.com>
2022-06-15 22:40:25 -0700

    added title and changed html and css

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const NASA_API_KEY = 'mlKrJKavjhseEuXEhWtBC7t9oluRqNLmIUFHvyyM'
 const QUERY = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`;
 
 fetch(QUERY)
-	.then((res) => res.json()).then((data) => {
-        const img = document.querySelector('.ex-pic');
-        const explanation = document.querySelector('.explanation')
+	.then((res) => res.json())
+    .then((data) => {
+        const img = document.querySelector('.daily-pic');
+        const explanation = document.querySelector('.explanation');
+        const title = document.querySelector('.title');
+
+        title.innerText = data.title;
         explanation.innerText = data.explanation;
         img.src = data.url;
         console.log(data)
